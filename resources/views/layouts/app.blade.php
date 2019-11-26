c9b65e8d115ff0d7ddc6c11f3fc4e863733c8f15
Barveey <bartek.chom@gmail.com>
2019-11-27 00:15:12 +0100

    zmiana tytulu aplikacji

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Kurs PHP') }}</title>
+    <title>Kurs PHP</title>
 
 
     <!-- Bootstrap css -->
