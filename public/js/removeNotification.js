a6201c999198ff65f3c31ca1f92999ae3f719952
Mescruu <mescruu@gmail.com>
2019-12-17 00:22:24 +0100

    poprawki dot systemu powiadomień

--- public/js/removeNotification.js
+++ public/js/removeNotification.js
@@ -1,13 +1,29 @@
 $(document).ready(function() {
 
+    let totalCount=$("#v-pills-notifications > div").length;
+    $('#notNumber').text(totalCount);
+
     var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
 
     // Delete record
     $(document).on("click", ".closeWarning", function () {
 
+
+
+
         if(confirm("Tej oparacji nie da się cofnąć!")) {
 
+            totalCount--;
+            $('#notNumber').text(totalCount);
+
+            if(totalCount==0){
+                $('#v-pills-notifications').text("<h4>Brak powiadomień</h4>");
+            }
+
+
             $("#not"+$(this).data('id')).hide(500);
+
+
 
             var delete_id = $(this).data('id');
             var el = this;
@@ -26,6 +42,16 @@ $(document).ready(function() {
     });
     $(document).on("click", ".closePrimary", function () {
 
+        totalCount--;
+        $('#notNumber').text(totalCount);
+
+        if(totalCount==0){
+            $('#v-pills-notifications').html(" <h2>\n" +
+                "                            Powiadomienia\n" +
+                "                        </h2>\n" +
+                "                        <hr>" +
+                "<h4>Brak powiadomień</h4>");
+        }
             $("#not"+$(this).data('id')).hide(500);
 
             var delete_id = $(this).data('id');
